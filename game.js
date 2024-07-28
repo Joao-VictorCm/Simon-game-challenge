1201d1415aebae4bc248f1a44e4ffdd1b40c5e5f
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-27 22:59:58 -0300

    add as funções que transmitem o sons dos botoes clicados

--- game.js
+++ game.js
@@ -2,6 +2,18 @@ const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 const userClickedPattern = [];
 
+$(".btn").click(function () {
+  // console.log("clicado");
+
+  var userChosenColour = this.id;
+
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+
+ playSound(userChosenColour)
+
+});
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   // console.log(randomNumber)
@@ -18,18 +30,12 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
-  var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
-  audio.play();
-
-  $(".btn").click(function (event) {
-    console.log("clicado");
-
-    var userChosenColour = event;
-    var userChosenColour = this.id;
-
-    userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
-  });
+playSound(randomChosenColour)
 }
 
 nextSequence();
+
+function playSound(name){
+  var audio = new Audio("./sounds/" + name + ".mp3");
+  audio.play();
+}
